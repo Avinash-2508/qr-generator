514c458f5ba25c2e50f2a44b6e4c10f7e8c55948
Avinash <avinash.ponneboina@gmail.com>
2025-07-30 21:15:09 +0530

    qr-image

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* 
 1. Use the inquirer npm package to get user input.
-2. Use the qr-image  to turn the user entered URL into a .
+2. Use the  to turn the user entered URL into a .
 3. Create a txt file to save the user input using the native fs .
 */
 
